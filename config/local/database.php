4194d9d06468610b1d669d74bac337ee2c101508
Marina Savelova <marina.savelova@pkp.vn.ua>
2015-05-07 13:08:23 +0300

    initial commit

---
+++ config/local/database.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+	'connections' => [
+
+		'mysql' => [
+			'driver'    => 'mysql',
+			'host'      => 'localhost',
+			'database'  => 'odezi',
+			'username'  => 'root',
+			'password'  => '123',
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+			'strict'    => false,
+		],
+	
+	],
+
+];
